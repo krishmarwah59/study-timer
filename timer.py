485ef697b741eee3e853d8f3596b6142ac261d05
“Krish <krish.marwah59@gmail.com>
2026-09-17 17:34:39 -0700

    add basic study timer

---
+++ timer.py
@@ -0,0 +1,11 @@
+import time
+
+minutes = int(input("How many minutes do you want to study? "))
+
+seconds = minutes * 60
+
+print("Study session started!")
+
+time.sleep(seconds)
+
+print("Time's up!")
